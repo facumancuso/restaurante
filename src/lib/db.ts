9a3ca23a7b1cb369f8dde5b5c0a0132b937ed8b0
Facundo Mancuso <105766388+facumancuso@users.noreply.github.com>
2025-07-24 17:34:15 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,46 @@
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+// }
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+
 import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
+
+const connectionString = process.env.DATABASE_URL!
+
+const pool = new Pool({ connectionString })
+
+const adapter = new PrismaNeon(pool)
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    adapter,
+  })
 }
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
 const prisma = globalThis.prisma ?? prismaClientSingleton()
 
-export default prisma
-
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+export default prisma
